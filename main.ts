9bea2baeab93f39462e9a35e5c50db176b91bc9a
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 23:00:17 +0500

    twentytwo assignment

---
+++ main.ts
@@ -0,0 +1,11 @@
+// Q:22 Intentional Error: If you haven’t received an array index error in one of your programs yet,
+    // try to make one happen. Change an index in one of your programs to produce an index error. 
+    // Make sure you correct the error before closing the program.
+
+    let arr =["Saeed", 25, "learner"];
+
+    console.log("produce an index error :");
+    console.log(arr [3] + `\n`);
+    console.log("Correction in an index error :");
+    console.log(arr [2]);
+
